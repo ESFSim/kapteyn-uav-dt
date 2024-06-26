2d80ce7d1256dc19ec0d4f36c69f5afdf6423235
Adam Weingram <adam@adamweingram.com>
2024-06-25 18:51:01 -0700

    Fix kwarg API update in ROS2 Node class

--- src/digitaltwin/launch/digitaltwin_launch.py
+++ src/digitaltwin/launch/digitaltwin_launch.py
@@ -2,21 +2,23 @@ from  launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    print("Running custom launcher...")
+    
     return LaunchDescription([
         Node(
             package='digitaltwin',
-            node_namespace='',
-            node_executable='asset.py',
-            node_name='uav_asset',
+            executable='asset.py',
+            name='uav_asset',
             output='screen',
             emulate_tty=True,
+            arguments=['--ros-args', '--log-level', 'debug'],
         ),
         Node(
             package='digitaltwin',
-            node_namespace='',
-            node_executable='twin.py',
-            node_name='uav_twin',
+            executable='twin.py',
+            name='uav_twin',
             output='screen',
             emulate_tty=True,
+            arguments=['--ros-args', '--log-level', 'debug'],
         ),
     ])
